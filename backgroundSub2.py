e6d339d69ea1f7e6b13d8f42586bb5f4e79486f6
AgreshB <agresh275@gmail.com>
2020-02-23 17:27:20 +0000

    Implemented handHist to make imageCapture more stable

--- backgroundSub2.py
+++ backgroundSub2.py
@@ -12,6 +12,7 @@ class backgroundSub2:
         # Background subtractor learning rate
         self.bgSubtractorLr = 0
         self.bgSubThreshold = 35
+        self.isHandHistCreated = False
 
         self.x0 = 0
         self.y0 = 0
@@ -19,6 +20,9 @@ class backgroundSub2:
         self.width = 0
         self.roiColor = (255, 0, 0)
         self.process = process('VLC')
+
+        self.xs = [6.0/20.0, 9.0/20.0, 12.0/20.0]
+        self.ys = [9.0/20.0, 10.0/20.0, 11.0/20.0]
 
         self.startPredict = False
         self.model = tf.keras.models.load_model("model/custModel-7.h5")
@@ -37,13 +41,58 @@ class backgroundSub2:
         self.y0 = int(frame_height*y)
         self.width = 400
         self.height = 400
+      
+    def histMasking(self, frame, handHist):
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        dst = cv2.calcBackProject([hsv], [0, 1], handHist, [0, 180, 0, 256], 1)
+
+        disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (21, 21))
+        cv2.filter2D(dst, -1, disc, dst)
+
+        ret, thresh = cv2.threshold(dst, 150, 255, cv2.THRESH_BINARY)
+
+        kernel = np.ones((5, 5), np.uint8)
+        thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=7)
+        # thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=5)
+        # thresh = cv2.dilate(thresh, kernel, iterations=5)
+        # thresh = cv2.erode(thresh, kernel, iterations=5)
+
+        thresh = cv2.merge((thresh, thresh, thresh))
+        return cv2.bitwise_and(frame, thresh)
+
+    def createHandHistogram(self, frame):
+        rows, cols, _ = frame.shape
+        hsvFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        roi = np.zeros([180, 20, 3], dtype=hsvFrame.dtype)
+        i = 0
+        for x in self.xs:
+            for y in self.ys:
+                x0, y0 = int(x*rows), int(y*cols)
+                roi[i*20:i*20 + 20, :, :] = hsvFrame[x0:x0 + 20, y0:y0 + 20, :]
+
+                i += 1
+        handHist = cv2.calcHist([roi], [0, 1], None, [180, 256], [0, 180, 0, 256])
+        return cv2.normalize(handHist, handHist, 0, 255, cv2.NORM_MINMAX)
+
+    def detectHand(self,frame,handHist):
+        roi = frame[self.y0:self.y0 + self.height, self.x0:self.x0 + self.width,:]
+        # Color masking
+        histMask = self.histMasking(roi, handHist)
+        #cv2.imshow("histMask", histMask)
+        # Background substraction
+        bgSubMask = self.bgSubMasking(roi)
+        #cv2.imshow("bgSubMask", bgSubMask)
+        # final mask take bitwise and of both
+        mask = cv2.bitwise_and(histMask, bgSubMask)
+        return mask
+
     
     def bgSubMasking(self, frame):
         fgmask = self.bgSubtractor.apply(frame, learningRate=self.bgSubtractorLr)
         kernel = np.ones((4, 4), np.uint8)
-        kernel2 = np.ones((5, 5), np.uint8)
-        fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel, iterations=3)
-        fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel2, iterations=3)
+        kernel2 = np.ones((3,3), np.uint8)
+        fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel2, iterations=3)
+        fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_CLOSE, kernel, iterations=3)
         return cv2.bitwise_and(frame, frame, mask=fgmask)
     
     def predictImage(self,image):
@@ -55,58 +104,94 @@ class backgroundSub2:
         prediction = self.model.predict(X)
         return np.argmax(prediction)
     
-    def checkAndRun(self,command):
-        if(self.frame_number % 5 == 0):
-            commandToRun = max(set(self.commandList) , key= self.commandList.count)
-            self.process.runCommand(commandToRun)
-            self.frame_number = 0
-            self.command =[]
-        else:
-            self.commandList.append(command)
-        
+    def runCommand(self):
+        commandToRun = max(set(self.commandList) , key= self.commandList.count)
+        self.process.checkAndRun(commandToRun)
+        self.frame_number = 0
+        self.commandList =[]
+        return commandToRun
+
+    def drawRect(self, frame):
+        roi = frame[self.y0:self.y0 + self.height, self.x0:self.x0 + self.width,:]
+        rows, cols, _ = roi.shape
+        frame1 = frame.copy()
+        for x in self.xs:
+            for y in self.ys:
+                x0, y0 = int(x*rows), int(y*cols)
+                cv2.rectangle(frame1, (self.y0+ y0, self.x0+ x0), (self.y0+ y0 + 20, self.x0+ x0 + 20), (0, 255, 0), 1)
+        return frame1 
+    
     def subtraction(self): 
         cap = cv2.VideoCapture(0)
         frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
         self.setupFrame(frame_width, frame_height)
+
+        #creating a black image for options window 
+        options = np.zeros(shape=[512, 512, 3], dtype=np.uint8)
+
+        cv2.namedWindow("Status",cv2.WINDOW_NORMAL)
         cv2.namedWindow('Original Frame',cv2.WINDOW_NORMAL)
-        cv2.namedWindow('cropped',cv2.WINDOW_NORMAL)
+
         cv2.namedWindow("threshold",cv2.WINDOW_NORMAL)
         cv2.moveWindow('Original Frame' ,6,27)
-        cv2.moveWindow('cropped' ,6,327)
+        cv2.moveWindow('Status' ,6,327)
         cv2.moveWindow('threshold' ,6,550)
+        gesture ='None'
+        handHist = None
         while(True):
-        
             # reading each frame
+            options = np.zeros(shape=[512, 512, 3], dtype=np.uint8)
             ret, frame = cap.read()
             cv2.rectangle(frame, (self.x0, self.y0), (self.x0 + self.width - 1, self.y0 + self.height - 1), self.roiColor, 2)
             roi = frame[self.y0:self.y0 + self.height, self.x0:self.x0 + self.width,:]
             self.frame_number +=1
 
-            if(self.isBgCaptured):
-                bgSubMask = self.bgSubMasking(roi)
-                grey = cv2.cvtColor(bgSubMask,cv2.COLOR_BGR2GRAY)
+            # if(self.isBgCaptured):
+            #     bgSubMask = self.bgSubMasking(roi)
+            #     grey = cv2.cvtColor(bgSubMask,cv2.COLOR_BGR2GRAY)
+            #     thresholded = cv2.threshold(grey, 20, 255, cv2.THRESH_BINARY)[1]
+            #     cv2.imshow("threshold",thresholded)
+
+            if(self.isHandHistCreated and self.isBgCaptured):
+                handMask = self.detectHand(frame,handHist)
+                grey = cv2.cvtColor(handMask,cv2.COLOR_BGR2GRAY)
                 thresholded = cv2.threshold(grey, 20, 255, cv2.THRESH_BINARY)[1]
                 cv2.imshow("threshold",thresholded)
-
+            elif not self.isHandHistCreated:
+                frame = self.drawRect(frame)
+            
             if(self.startPredict):
                 resImage = cv2.resize(thresholded,(200,200))
-                gesture =self.class_names[self.predictImage(thresholded)]
+                gesture1 =self.class_names[self.predictImage(thresholded)]
+                self.commandList.append(gesture1)
+                if (self.frame_number % 5 == 0):
+                    self.frame_number =0
+                    #gesture = max(set(self.commandList) , key= self.commandList.count)
+                    if (self.startExecute):
+                        gesture = self.runCommand()
                 cv2.putText(frame,gesture,(10,100),cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,0),cv2.LINE_AA)
-                if (self.startExecute):
-                    self.checkAndRun(gesture)
-                    
+
+            #print options window  
+            #cv2.putText(options,self.printMenu(),(5,5),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255))
+            cv2.putText(options,"Background Capture :{}".format(self.isBgCaptured),(10,100),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255))
+            cv2.putText(options,"Prediction started :{}".format(self.startPredict),(10,200),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255))
+            cv2.putText(options,"Execution started  :{}".format(self.startExecute),(10,300),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255))
+            cv2.putText(options,"Hist Capture       :{}".format(self.isHandHistCreated),(10,400),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,255))
 
             cv2.imshow('Original Frame',frame)
-            cv2.imshow('cropped',roi)
+            cv2.imshow('Status',options)
             k = cv2.waitKey(1) & 0xFF
             if k == ord('b'):
                 self.bgSubtractor = cv2.createBackgroundSubtractorMOG2(10, self.bgSubThreshold,detectShadows=False)
                 self.isBgCaptured = True
+            elif k== ord('z'):
+                self.isHandHistCreated = True
+                handHist = self.createHandHistogram(roi)
             elif k ==ord('q'):
-                cv2.imwrite('capture.jpg', frame)
                 break
             elif k ==ord('p'):
+                self.frame_number= 0
                 self.startPredict = not self.startPredict
             elif k ==ord('e'):
                 self.frame_number = 0
@@ -121,20 +206,33 @@ class backgroundSub2:
             elif k == ord('a'):
                 #moving roi to left
                 self.x0 -= 5
+
         cap.release()
         cv2.destroyAllWindows()
 
     def printMenu(self):
-        print("-------------------------------------------")
-        print("               HANDY GEST                  ")
-        print("-------------------------------------------")
-        print("OPTIONS:")
-        print("To Caputure BAckground (or re-calib): Press b")
-        print("To move the ROI                     : Use w a s d ")
-        print("To TOGGLE PREDICTION                : Press p")
-        print("To TOGGLE EXECUTION                 : Press e")
+        menu =""
+        # print("-------------------------------------------")
+        # print("               HANDY GEST                  ")
+        # print("-------------------------------------------")
+        # print("OPTIONS:")
+        # print("To Caputure BAckground (or re-calib): Press b")
+        # print("To move the ROI                     : Use w a s d ")
+        # print("To TOGGLE PREDICTION                : Press p")
+        # print("To TOGGLE EXECUTION                 : Press e")
+        menu +="-------------------------------------------\n"
+        menu +="               HANDY GEST                  \n"
+        menu +="-------------------------------------------\n"
+        menu +="OPTIONS:\n"
+        menu +="To Caputure BAckground (or re-calib): Press b\n"
+        menu +="To move the ROI                     : Use w a s d\n"
+        menu +="To TOGGLE PREDICTION                : Press p\n"
+        menu +="To TOGGLE EXECUTION                 : Press e\n"
+        menu +="To Capture HandHistogram            : Press z\n"
+        print(menu)
+        return menu
 
 if __name__ == "__main__":
     detect = backgroundSub2()
-    detect.printMenu()
+    menu = detect.printMenu()
     detect.subtraction()
--- execute.py
+++ execute.py
@@ -10,6 +10,7 @@ class process:
         self.isMute = False
         self.app = appName
         self.previous = ""
+        self.exceptionGest = ['L','index']
 
 
     def openApp(self):
@@ -18,39 +19,46 @@ class process:
 
     def runCommand(self, command):
         if command =='None':
+            self.previous = command
             return
         elif command =='fist':
             # mute if fist
-            if self.isMute ==False:
-                pgui.hotkey("option","command","down")
-                self.isMute = True
-            return
+            #if self.isMute ==False:
+            pgui.hotkey("option","command","down")
+            #self.isMute = True
         elif command =='palm':
             # unmute if palm
-            if self.isMute ==True:
-                pgui.hotkey("option","command","down")
-                self.isMute = False
-            return
+            #if self.isMute ==True:
+            pgui.hotkey("option","command","down")
+            #self.isMute = False
         elif command == 'peace':
             # pause if palm 
-            if self.isPause == False:
-                pgui.hotkey("space")
-                self.isPause = True
-            return
-        elif command =='ok':
-            # play if ok
-            if self.isPause == True :
-                pgui.hotkey("space")
-                self.isPause = False
-            return
-        elif command =='L':
-            # volume up if L
-            pgui.hotkey("up")
-            return
+            #if self.isPause == False:
+            pgui.hotkey("space")
+            #self.isPause = True
         elif command =='index':
+            # play if index
+            #if self.isPause == True :
+            pgui.hotkey("space")
+            #self.isPause = False
+        elif command =='ok':
+            # volume up if ok
+            pgui.hotkey("up")
+        elif command =='L':
             # volume down if index 
             pgui.hotkey("down")
-            return
+        self.previous = command
     
+    def checkAndRun(self,command):
+        print("Command : {0} ,  Prev : {1}".format(command,self.previous))
+        if command in self.exceptionGest:
+            self.runCommand(command)
+            return
+        if command == self.previous:
+            return
+        else:
+            self.runCommand(command)
+        
+
 
 
